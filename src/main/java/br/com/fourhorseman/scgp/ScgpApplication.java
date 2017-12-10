7105f6d2667cc57dcb30409a2c5fe333e69af276
Ricardo L Pansonato <ricardolp.bsi@gmail.com>
2017-12-10 12:58:44 -0200

    Initial commit

---
+++ src/main/java/br/com/fourhorseman/scgp/ScgpApplication.java
@@ -0,0 +1,12 @@
+package br.com.fourhorseman.scgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgpApplication.class, args);
+	}
+}
